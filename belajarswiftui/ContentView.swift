e9210372b14df443320dfba5cc59e3c8e09a5df0
Fauzan Ghozi Mubarak <fauzanghozi56@gmail.com>
2023-07-01 11:01:07 +0700

    update content body verif skpd dan ignore safeare top

--- belajarswiftui/ContentView.swift
+++ belajarswiftui/ContentView.swift
@@ -10,12 +10,17 @@ import SlidingTabView
 struct ContentView: View {
     var body: some View {
         VStack{
-            RectangtleTop()
-            tabIndexSKPD()
-                  
-            Spacer()
-            
+            Color(hex: 0x264653).edgesIgnoringSafeArea(.top)
+                .frame(height: 0)
+            VStack{
+                RectangtleTop()
+                tabIndexSKPD()
+                Spacer()
+                
+            }
+            .padding(EdgeInsets(top: -20, leading: 0, bottom: 0, trailing: 0))
         }
+  
       
     }
 }
@@ -23,20 +28,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct tabIndexSKPD : View{
-    @State private var tabIndex = 0
-    var body: some View{
-        VStack{
-            SlidingTabView(selection: $tabIndex, tabs: ["SKPD","E-SPTPD"], animation: .easeInOut)
-            if(tabIndex == 0){
-                ViewSkpd()
-            } else if tabIndex == 1 {
-                ViewSptpd()
-            }
-        }
     }
 }
 
@@ -73,24 +64,55 @@ struct RectangtleTop: View {
     }
 }
       
+struct tabIndexSKPD : View{
+    @State private var tabIndex = 0
+    var body: some View{
+        VStack{
+            SlidingTabView(selection: $tabIndex, tabs: ["SKPD","E-SPTPD"], animation: .easeInOut)
+            if(tabIndex == 0){
+                ViewSkpd()
+            } else if tabIndex == 1 {
+                ViewSptpd()
+            }
+        }
+    }
+}
 
 struct ViewSkpd : View {
     var body: some View {
         VStack{
             SearchView()
-            Text("Haii SKPD")
+            ScrollView {
+                    VStack(spacing: 16) {
+                        ForEach(0..<10) { index in
+                            ContentBodyView()
+                        }
+                    }
+                    .padding()
+                }
         }
+        .frame(height: .infinity)
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 40, trailing: 0))
     }
 }
-
 struct ViewSptpd : View {
     var body: some View {
         VStack{
             SearchView()
-            Text("Haii E-SPTPD")
+            ScrollView {
+                    VStack(spacing: 16) {
+                        ForEach(0..<10) { index in
+                            ContentBodyView()
+                        }
+                    }
+                    .padding()
+                }
         }
+        .frame(height: .infinity)
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 40, trailing: 0))
     }
 }
+
 
 struct SearchView: View {
     @State private var searchText = ""
@@ -121,6 +143,48 @@ struct SearchView: View {
         }
     }
 }
+
+
+struct ContentBodyView: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+                .foregroundColor(Color.white)
+                .cornerRadius(10)
+                .frame(height: 150)
+                .background(Color.white)
+                .cornerRadius(10)
+                .overlay(
+                    HStack{
+                        VStack(alignment: .leading, spacing: 8) {
+                            
+                            Text("No SKPD: A-12345678")
+                                .font(.system(size: 12))
+                            Text("NPWPD: 1234.12.123")
+                                .font(.system(size: 12))
+                            Text("Nama Wajib Pajak: Hotel ABC")
+                                .font(.system(size: 12))
+                            Text("Masa Pajak: Desember 2022")
+                                .font(.system(size: 12))
+                            Text("Jumlah Pajak: Rp. 600.000")
+                                .font(.system(size: 12))
+
+                            }
+                            .foregroundColor(.black)
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 16)
+                            
+                        Spacer()
+                    }
+             
+                      
+                )
+                .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+    
+        }
+    }
+}
+
 
 
 extension Color {
